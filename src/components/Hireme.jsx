dfb42961adc49160b659a960e1cd0ea8b0d43096
JATENCIA <atenciajaramilloj@gmail.com>
2023-07-24 15:54:19 -0500

    update

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import hireMe from "../assets/images/naruto.png";
+import React from 'react'
+import hireMe from '../assets/images/naruto.png'
 const Hireme = () => {
   return (
-    <section id="hireme" className="py-10 px-3 text-white">
-      <div className="text-center">
-        <h3 className="text-4xl font-semibold">
-          Hire <span className="text-cyan-600">Me</span>
+    <section id='hireme' className='py-10 px-3 text-white'>
+      <div className='text-center'>
+        <h3 className='text-4xl font-semibold'>
+          Hire <span className='text-cyan-600'>Me</span>
         </h3>
-        <p className="text-gray-400 mt-3 text-lg">Do you have any work?</p>
+        <p className='text-gray-400 mt-3 text-lg'>Do you have any work?</p>
       </div>
-      <div className="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
+      <div className='bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center'>
         <div>
-          <h2 className="text-2xl font-semibold">
+          <h2 className='text-2xl font-semibold'>
             Do you want any work from me?
           </h2>
-          <p className=" text-justify max-w-lg text-sm mt-4 text-gray-200 leading-7">
+          <p className=' text-justify max-w-lg text-sm mt-4 text-gray-200 leading-7'>
             Are you looking for a Full Stack developer with knowledge of
             technologies such as React, Node, Express, Redux, MongoDB, Mongoose,
             PostgreSQL, Sequelize, Git, GitHub, HTML, and CSS? Look no further!
@@ -25,18 +25,20 @@ const Hireme = () => {
             bring value to any team. Feel free to reach out to me for any
             challenge your team may have!
           </p>
-          <a href="#contact">
-            <button className="btn-primary mt-10">Say Hello</button>
+          <a href='#contact'>
+            <button className='btn-primary mt-10 hover:bg-cyan-700 transition'>
+              Say Hello
+            </button>
           </a>
         </div>
         <img
           src={hireMe}
-          alt=""
-          className="lg:h-[32rem] h-80 lg:absolute bottom-0 -right-3 object-cover"
+          alt=''
+          className='lg:h-[32rem] h-80 lg:absolute bottom-0 -right-3 object-cover'
         />
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hireme;
+export default Hireme
